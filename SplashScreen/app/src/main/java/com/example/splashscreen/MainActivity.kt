d23e7466727e0f4e673d27b81f7080b29b2e7c65
Arunabha Mandal <arunabhamandal461@gmail.com>
2023-08-02 00:28:51 +0530

    day 15 added - gradient design/ ui, ux etc.

--- SplashScreen/app/src/main/java/com/example/splashscreen/MainActivity.kt
+++ SplashScreen/app/src/main/java/com/example/splashscreen/MainActivity.kt
@@ -4,15 +4,6 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.splashscreen.ui.theme.SplashScreenTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,7 +12,7 @@ class MainActivity : ComponentActivity() {
 
         // Explore this
         Handler().postDelayed({
-            val intent = Intent(this, SecondActivity::class.java)
+            val intent = Intent(this, Signup::class.java)
             startActivity(intent)
             finish()
         }, 3000)
--- SplashScreen/app/src/main/java/com/example/splashscreen/Signup.kt
+++ SplashScreen/app/src/main/java/com/example/splashscreen/Signup.kt
@@ -3,9 +3,9 @@ package com.example.splashscreen
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class SecondActivity : AppCompatActivity() {
+class Signup : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_second)
+        setContentView(R.layout.activity_signup)
     }
 }
